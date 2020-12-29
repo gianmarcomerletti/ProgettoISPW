5967a4de90987d4f1360e59f045d8c43f6e8cc66
gianmarco <giamp@GianmarcoPC.mshome.net>
2020-12-29 13:58:46 +0100

    #16.2

--- src/main/java/com/gianmarco/merletti/progetto_ispw/logic/util/Constants.java
+++ src/main/java/com/gianmarco/merletti/progetto_ispw/logic/util/Constants.java
@@ -6,49 +6,49 @@ public final class Constants {
 		// private constructor
 	}
 
-	public static String USERNAME_DB = "root";
-	public static String PSW_DB = "2202";
-	public static String URL_DB = "jdbc:mysql://localhost:3306/progettoispw?serverTimezone=UTC";
+	public final static String USERNAME_DB = "root";
+	public final static String PSW_DB = "2202";
+	public final static String URL_DB = "jdbc:mysql://localhost:3306/progettoispw?serverTimezone=UTC";
 
-	private static String SELECT_ALL_FROM = "SELECT * FROM ";
-	private static String INSERT_INTO = "INSERT INTO ";
-	private static String DELETE_FROM = "DELETE FROM ";
+	private final static String SELECT_ALL_FROM = "SELECT * FROM ";
+	private final static String INSERT_INTO = "INSERT INTO ";
+	private final static String DELETE_FROM = "DELETE FROM ";
 
-	private static String TABLE_CITY = "city";
-	private static String TABLE_EVENT = "event";
-	private static String TABLE_JOIN = "organization";
-	private static String TABLE_REQUEST = "request";
-	private static String TABLE_USER = "user";
+	private final static String TABLE_CITY = "city";
+	private final static String TABLE_EVENT = "event";
+	private final static String TABLE_JOIN = "organization";
+	private final static String TABLE_REQUEST = "request";
+	private final static String TABLE_USER = "user";
 
-	public static String COLUMN_NAME = "name";
-	public static String COLUMN_LATITUDE = "latitude";
-	public static String COLUMN_LONGITUDE = "longitude";
-	public static String COLUMN_BORDERS = "borders";
-	public static String COLUMN_IDEVENT = "idevent";
-	public static String COLUMN_TITLE = "title";
-	public static String COLUMN_DESCRIPTION = "description";
-	public static String COLUMN_CREATION_DATE = "creation_date";
-	public static String COLUMN_DATE = "date";
-	public static String COLUMN_TIME = "time";
-	public static String COLUMN_ADDRESS = "address";
-	public static String COLUMN_CITY = "city";
-	public static String COLUMN_ORGANIZER = "organizer";
-	public static String COLUMN_LEVEL = "level";
-	public static String COLUMN_DISTANCE = "distance";
-	public static String COLUMN_TYPE = "type";
-	public static String COLUMN_IDREQUEST = "idrequest";
-	public static String COLUMN_USER = "user";
-	public static String COLUMN_MESSAGE = "message";
-	public static String COLUMN_STATUS = "status";
-	public static String COLUMN_PASSWORD = "password";
-	public static String COLUMN_USERNAME = "username";
-	public static String COLUMN_SURNAME = "surname";
+	public final static String COLUMN_NAME = "name";
+	public final static String COLUMN_LATITUDE = "latitude";
+	public final static String COLUMN_LONGITUDE = "longitude";
+	public final static String COLUMN_BORDERS = "borders";
+	public final static String COLUMN_IDEVENT = "idevent";
+	public final static String COLUMN_TITLE = "title";
+	public final static String COLUMN_DESCRIPTION = "description";
+	public final static String COLUMN_CREATION_DATE = "creation_date";
+	public final static String COLUMN_DATE = "date";
+	public final static String COLUMN_TIME = "time";
+	public final static String COLUMN_ADDRESS = "address";
+	public final static String COLUMN_CITY = "city";
+	public final static String COLUMN_ORGANIZER = "organizer";
+	public final static String COLUMN_LEVEL = "level";
+	public final static String COLUMN_DISTANCE = "distance";
+	public final static String COLUMN_TYPE = "type";
+	public final static String COLUMN_IDREQUEST = "idrequest";
+	public final static String COLUMN_USER = "user";
+	public final static String COLUMN_MESSAGE = "message";
+	public final static String COLUMN_STATUS = "status";
+	public final static String COLUMN_PASSWORD = "password";
+	public final static String COLUMN_USERNAME = "username";
+	public final static String COLUMN_SURNAME = "surname";
 
 
-	public static String SQL_FIND_CITY = SELECT_ALL_FROM + TABLE_CITY
+	public final static String SQL_FIND_CITY = SELECT_ALL_FROM + TABLE_CITY
 				+ " WHERE (" + COLUMN_NAME + "=?);";
 
-	public static String SQL_ADD_EVENT = INSERT_INTO + TABLE_EVENT
+	public final static String SQL_ADD_EVENT = INSERT_INTO + TABLE_EVENT
 			+ " (" + COLUMN_TITLE + ", "
 			+ COLUMN_DESCRIPTION + ", "
 			+ COLUMN_CREATION_DATE + ", "
