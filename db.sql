4c54aa6680d156ff227008c0a30b94038eb6d9be
gianmarco <giamp@GianmarcoPC.mshome.net>
2021-01-14 11:34:18 +0100

    #26.5

--- db.sql
+++ db.sql
@@ -1,7 +1,7 @@
 CREATE USER 'test'@'localhost' IDENTIFIED BY 'test';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'test'@'localhost';
 
-CREATE DATABASE  IF NOT EXISTS `progettoispw` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `progettoispw` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `progettoispw`;
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
